d181457500ed579f2419e265f13f24b2dfa002f7
mstuder <ms@studer-raimann.ch>
2019-04-09 16:53:29 +0200

    set ILIAS Verson 5.4

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'dhbwautolo';
 $version = '0.0.8';
 $ilias_min_version = '5.2.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'studer + raimann ag';
 $responsible_mail = 'info@studer-raimann.ch';
