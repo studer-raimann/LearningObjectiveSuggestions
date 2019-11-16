a97c377989ad517974de91eddcc16c52254b4d13
mstuder <ms@studer-raimann.ch>
2019-11-16 15:45:43 +0100

    new plugin version

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## [0.1.0]
+* Only show e-mentorings of specific group
+* Only show exercises of specific group
--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id = 'dhbwautolo';
-$version = '0.0.8';
+$version = '0.1.0';
 $ilias_min_version = '5.2.0';
 $ilias_max_version = '5.4.999';
 $responsible = 'studer + raimann ag';
