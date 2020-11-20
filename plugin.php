52d9087cb31e773fc241e80e6e347c77145cef06
mstuder <ms@studer-raimann.ch>
2020-11-20 18:16:34 +0100

    5.4

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'dhbwautolo';
 $version = '0.1.7';
-$ilias_min_version = '6.0.0';
+$ilias_min_version = '5.4.0';
 $ilias_max_version = '6.9.999';
 $responsible = 'studer + raimann ag';
 $responsible_mail = 'info@studer-raimann.ch';
