3cd3d108784e96effa8f3dc02dc4f6ec7ca66060
Erik Mokroß <em@studer-raimann.ch>
2021-06-17 12:51:33 +0200

    fix duplicate suggestions

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -31,3 +31,31 @@ foreach(SRAG\ILIAS\Plugins\LearningObjectiveSuggestions\Suggestion\LearningObjec
 	$sug->save();
 }
 ?>
+
+<#5>
+<?php
+    GLobal $DIC;
+
+    $DIC->database()->query("DELETE t
+     FROM alo_score t JOIN
+          (SELECT user_id, course_obj_id, objective_id, min(score) AS min_score
+           FROM alo_score t
+           GROUP BY user_id, course_obj_id, objective_id
+          ) tt
+          USING (user_id, course_obj_id, objective_id)
+    WHERE score > min_score;");
+
+    $DIC->database()->query("DELETE t
+         FROM alo_suggestion t JOIN
+              (SELECT user_id, course_obj_id, objective_id, min(created_at) AS min_date
+               FROM alo_suggestion t
+               GROUP BY user_id, course_obj_id, objective_id
+              ) tt
+              USING (user_id, course_obj_id, objective_id)
+        WHERE created_at > min_date;");
+
+
+//    $DIC->database()->query("ALTER TABLE alo_score ADD UNIQUE unique_index(user_Id, course_obj_id, objective_id)");
+//    $DIC->database()->query("ALTER TABLE alo_suggestion ADD UNIQUE unique_index(user_Id, course_obj_id, objective_id)");
+
+?>
